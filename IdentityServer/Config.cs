28068a67855f9bf5c847a05c430f38cbfb8903f6
ilio78 <36421443+ilio78@users.noreply.github.com>
2019-07-15 14:25:14 +0200

    More configuration changes

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -52,10 +52,13 @@ namespace IdentityServer
                 AllowedGrantTypes = GrantTypes.Implicit,
 
                 // where to redirect to after login
-                RedirectUris = { "http://localhost:5002/signin-oidc" },
+                //RedirectUris = { "http://localhost:5002/signin-oidc" },
+                RedirectUris = { "http://eimas-clientapp1.azurewebsites.net/signin-oidc" },
+
 
                 // where to redirect to after logout
-                PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+                //PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+                PostLogoutRedirectUris = { "http://eimas-clientapp1.azurewebsites.net/signout-callback-oidc" },
 
                 AllowedScopes = new List<string>
                 {
